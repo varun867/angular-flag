a72c4adaa46a795f97a4d27f5545a98060519268
varun867 <varun867@gmail.com>
2020-01-04 07:45:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-u8jk3g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-u8jk3g)
---
+++ src/flags-listing/flags-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flags-listing',
+  templateUrl: './flags-listing.component.html',
+  styleUrls: ['./flags-listing.component.css']
+})
+export class FlagsListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
