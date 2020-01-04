12b8c10eaadc796c710bb14df5b4217ae3c3f7d4
varun867 <varun867@gmail.com>
2020-01-04 07:57:42 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+import {environment} from '../environments';
 import { FlagsListingComponent } from '../flags-listing/flags-listing.component';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireModule } from 'angularfire2'
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule
+    ],
   declarations: [ AppComponent, HelloComponent, FlagsListingComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/environments.ts
+++ src/environments.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    databaseURL: YOUR_DATABASE_URL,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: "",
-    messagingSenderId: YOUR_MESSAGING_SENDER_ID
-  }
+    production: false,
+    firebase: {
+        apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        authDomain: 'project-flag.firebaseapp.com',
+        databaseURL: 'https://project-flag.firebaseio.com',
+        projectId: 'project-flag',
+        storageBucket: 'project-flag.appspot.com',
+        messagingSenderId: 'ZZZZZZZZZZZZ'
+    }
 };
